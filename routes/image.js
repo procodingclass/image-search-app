333614dc200324f82cb35876bfa169ecf9c8d2b6
procodingclass <88050205+procodingclass@users.noreply.github.com>
2023-02-16 16:40:19 +0530

    Update image.js

--- routes/image.js
+++ routes/image.js
@@ -23,19 +23,47 @@ router.get("/:imageName",
             images = []
             for(i=0; i<3;i++)
             {
-               images[i] =  await fetch(imageURL+imageName)
-                            .then(response => response.url)
-                            .then(imageUrl=> {
-                                console.log(imageUrl)
-                                return imageUrl
-                            })
-                            .catch(err => console.error(err));
+             
+                images[i] =  await fetch(imageURL+imageName)
+                        .then(response => response.url)
+                        .then(imageUrl=> {
+                            console.log(imageUrl)
+                            return imageUrl
+                        })
+                        .catch(err => console.error(err));
+
+                setTimeout(wait,400);
             }
+
+            function wait()
+            {
+                console.log("waiting")
+            }
+
+            // var i = 0;                  //  set your counter to 1
+
+            // async function myLoop() {         //  create a loop function
+            //  await setTimeout(async function() {   //  call a 3s setTimeout when the loop is called
+            //     images[i] = await fetch(imageURL+imageName)
+            //                 .then(response => response.url)
+            //                 .then(imageUrl=> {
+            //                     console.log(imageUrl)
+            //                     return imageUrl
+            //                 })
+            //                 .catch(err => console.error(err));
+            //     i++;                    //  increment the counter
+            //     if (i < 3) {           //  if the counter < 10, call the loop function
+            //     await myLoop();             //  ..  again which will trigger another 
+            //     }                       //  ..  setTimeout()
+            // }, 1000)
+            // }
+
+            // await myLoop();                   //  start the loop
             
             return res.status(200).json({
                 "responseMessage": "Response given as image array and info", 
                 "images": images, 
-                "info" : "This is the text returned for the "+imageName});
+                "info" : "This is the text returned for the "+ await imageName});
             
         }
         catch (error) {
